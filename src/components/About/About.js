1575bc0d931a25a8ec4e66648dbb50287bbc15fe
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2023-05-13 01:10:01 +0530

    Update About tab

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -21,7 +21,14 @@ function About() {
               paddingBottom: "50px",
             }}
           >
-            <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
+            <h1
+              style={{
+                fontSize: "2.1em",
+                paddingBottom: "20px",
+                textAlign: "left",
+                marginLeft: "15px",
+              }}
+            >
               Know Who <strong className="purple">I'M</strong>
             </h1>
             <Aboutcard />
