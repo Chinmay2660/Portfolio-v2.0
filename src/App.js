12169a20fcdb2b607de744845497f7e8a6482cb8
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2023-05-14 01:00:38 +0530

    Add Education.js and Experience.js

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import Experience from "./components/Experience/Experience";
-// import Education from "./components/Education/Education";
+import Experience from "./components/Experience/Experience";
+import Education from "./components/Education/Education";
 import Resume from "./components/Resume/ResumeNew";
 import {
   HashRouter as Router,
@@ -40,8 +40,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
-          {/* <Route path="/experience" element={<Experience />} />
-          <Route path="/education" element={<Education />} /> */}
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/education" element={<Education />} />
           <Route path="/resume" element={<Resume />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
