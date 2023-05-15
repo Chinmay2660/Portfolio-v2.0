f1279a4008b1f84bc776c1f7c8ceea2de00be7f7
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2023-05-15 20:40:19 +0530

    Add ContactForm.js

---
+++ src/components/ContactForm.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+const ContactForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform any necessary form submission logic heres
+    console.log("Submitted:", name, email, message);
+    // Clear form fields
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <textarea
+        placeholder="Message"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      ></textarea>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default ContactForm;
