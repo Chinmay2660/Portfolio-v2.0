fa46be9aa20ad4296bd1219e1fd75aa1539d6fcc
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2023-05-17 22:18:28 +0530

    Add install option for app

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,11 @@
+// import React from "react";
+// import { Container, Row, Col } from "react-bootstrap";
+// import Particle from "../Particle";
+
+// function Contact() {
+//     return (
+     
+//     );
+// }
+
+// export default Contact;
