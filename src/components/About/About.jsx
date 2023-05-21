e24e80e0a8fa901ef0bba2813153bfe5d0377360
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2023-05-21 14:03:10 +0530

    Code refactor

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -35,7 +35,10 @@ function About() {
           </Col>
           <Col
             md={5}
-            style={{ paddingTop: "120px", paddingBottom: "50px" }}
+            style={{
+              paddingTop: "120px",
+              paddingBottom: "50px"
+            }}
             className="about-img"
           >
             <img src={laptopImg} alt="about" className="img-fluid" />
@@ -50,7 +53,12 @@ function About() {
         <h1 className="project-heading">
           <strong className="purple">Tools</strong> I use
         </h1>
+
         <Toolstack />
+
+        <h1 className="project-heading" >
+        Days I <strong className="purple">Code</strong>
+        </h1>
 
         <Github />
       </Container>
