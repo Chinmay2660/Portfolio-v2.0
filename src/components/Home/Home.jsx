8ac0e71be6b669b9a2272ccadc6a97a3e11177e6
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2023-06-01 18:03:41 +0530

    Delete img

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import homeLogo from "../../Assets/home-main.svg";
 import Home2 from "./Home2";
-// import Type from "./Type";
+import Type from "./Type";
 import Resume from "../Resume/Resume";
 import HireMeButton from "../HireMe/HireMeButton";
 
